2eb85dd1c40bf002ee5b8a4dde0ca352a2834545
Ruby Jau <jau.ruby@gmail.com>
2023-09-20 23:24:46 +0800

    doing scan function

--- Goldence/Controller/AddPage/AddBook/AddByISBN/Segments/ScanViewController.swift
+++ Goldence/Controller/AddPage/AddBook/AddByISBN/Segments/ScanViewController.swift
@@ -6,24 +6,71 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ScanViewController: UIViewController {
-
+class ScanViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var barView: UIView!
+    var captureSession: AVCaptureSession?
+    var inputLayer: AVCaptureVideoPreviewLayer?
+    var barCodeFrameView: UIView?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // try to find back camera
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Camera device was not found")
+            return
+        }
+        setUpVideoCapture(captureDevice: captureDevice)
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession?.startRunning()
+        }
+        view.bringSubviewToFront(textLabel)
+        // setting frame view on barcode
+        barCodeFrameView = UIView()
+        if let barcodeFrameView = barCodeFrameView {
+            barcodeFrameView.layer.borderColor = UIColor.blue.cgColor
+            barcodeFrameView.layer.borderWidth = 1
+            view.addSubview(barcodeFrameView)
+            view.bringSubviewToFront(barcodeFrameView)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpVideoCapture(captureDevice: AVCaptureDevice) {
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(cameraInput)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession!.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            // setting the type of metadata we are trying to read
+            // ISBN barcodes are of type ean13
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.ean13]
+            inputLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            inputLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            inputLayer?.frame = barView.layer.bounds
+            barView.layer.addSublayer(inputLayer!)
+        } catch {
+            print(error)
+        }
     }
-    */
-
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects.count == 0 {
+            barCodeFrameView?.frame = CGRect.zero
+            textLabel.text = "No barcode is found"
+        } else {
+            // found some code
+            let metadataObj = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
+            if metadataObj?.type == AVMetadataObject.ObjectType.ean13 {
+                // found ISBN barcode
+                let barCodeObject = inputLayer?.transformedMetadataObject(for: metadataObj!)
+                barCodeFrameView?.frame = barCodeObject!.bounds
+                if metadataObj?.stringValue != nil {
+                    textLabel.text = metadataObj?.stringValue
+                    print(metadataObj?.stringValue! as Any)
+                }
+            }
+        }
+    }
 }
