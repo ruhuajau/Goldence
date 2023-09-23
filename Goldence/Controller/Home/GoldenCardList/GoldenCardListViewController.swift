04a935fd6b195b02b490d268331c9c1a0f032560
Ruby Jau <jau.ruby@gmail.com>
2023-09-23 17:48:10 +0800

    GoldenCardList can be shown

---
+++ Goldence/Controller/Home/GoldenCardList/GoldenCardListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GoldenCardListViewController.swift
+//  Goldence
+//
+//  Created by 趙如華 on 2023/9/23.
+//
+
+import UIKit
+
+class GoldenCardListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        if indexPath.section == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "AddGoldenCardCell", for: indexPath)
+        } else {
+            cell = tableView.dequeueReusableCell(withIdentifier: "GoldenCardCell", for: indexPath)
+        }
+        return cell
+    }
+}
