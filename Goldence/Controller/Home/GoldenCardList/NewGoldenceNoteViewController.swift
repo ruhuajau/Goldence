90d4f8a6a45229fdd0aa16599ffcc801c75fbce4
Ruby Jau <jau.ruby@gmail.com>
2023-09-25 16:31:45 +0800

    add save button

--- Goldence/Controller/Home/GoldenCardList/NewGoldenceNoteViewController.swift
+++ Goldence/Controller/Home/GoldenCardList/NewGoldenceNoteViewController.swift
@@ -9,37 +9,35 @@ import UIKit
 import VisionKit
 
 class NewGoldenceNoteViewController: UIViewController {
-    
     private var scanButton = ScanButton(frame: .zero)
+    private var saveButton = SaveButton(frame: .zero)
     private var scanImageView = ScanImageView(frame: .zero)
     private var scanTextView = ScanTextView(frame: .zero, textContainer: nil)
     private var scrollView = UIScrollView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configure()
     }
-    
     private func configure() {
         view.addSubview(scrollView)
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
         view.addSubview(scanTextView)
         view.addSubview(scanButton)
-        
+        view.addSubview(saveButton)
         let padding: CGFloat = 16
-        
         NSLayoutConstraint.activate([
             scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
             scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
             scrollView.bottomAnchor.constraint(equalTo: scanTextView.topAnchor, constant: -padding),
-            
             scanButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
-            scanButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+            scanButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -200),
             scanButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
             scanButton.heightAnchor.constraint(equalToConstant: 50),
+            saveButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 250),
+            saveButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+            saveButton.heightAnchor.constraint(equalToConstant: 50),
             scanTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
             scanTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
             scanTextView.bottomAnchor.constraint(equalTo: scanButton.topAnchor, constant: -padding),
