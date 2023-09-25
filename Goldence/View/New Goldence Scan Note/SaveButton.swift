3a61d75b0cff52eff2ef35c0d4e53289b62714c2
Ruby Jau <jau.ruby@gmail.com>
2023-09-25 16:31:45 +0800

    add save button

---
+++ Goldence/View/New Goldence Scan Note/SaveButton.swift
@@ -0,0 +1,30 @@
+//
+//  SaveButton.swift
+//  Goldence
+//
+//  Created by 趙如華 on 2023/9/25.
+//
+import Foundation
+import UIKit
+
+class SaveButton: UIButton{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+        
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+        
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        setTitle("Save", for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        titleLabel?.textColor = .white
+        layer.cornerRadius = 7.0
+        backgroundColor = UIColor.systemIndigo
+    }
+}
