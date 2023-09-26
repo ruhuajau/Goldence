a39674f2a062c65b27f4531372d026271b4451ae
Ruby Jau <jau.ruby@gmail.com>
2023-09-26 18:02:57 +0800

    ready to do UI

---
+++ Goldence/Controller/CustomTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarController.swift
+//  Goldence
+//
+//  Created by 趙如華 on 2023/9/26.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        tabBar.tintColor = .black
+    }
+}
