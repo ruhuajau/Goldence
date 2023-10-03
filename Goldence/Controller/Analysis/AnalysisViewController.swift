6b9e2a9c3f070f79cf04f2c42a4e92164ec25ba9
Ruby Jau <jau.ruby@gmail.com>
2023-10-03 21:39:27 +0800

    show abbreviate date

--- Goldence/Controller/Analysis/AnalysisViewController.swift
+++ Goldence/Controller/Analysis/AnalysisViewController.swift
@@ -19,7 +19,11 @@ class AnalysisViewController: UIViewController {
             super.viewDidLoad()
 
             fetchDataForLineChart()
+            lineChartView.extraTopOffset = 50 // Add extra top offset (adjust as needed)
+            lineChartView.extraBottomOffset = 50 // Add extra bottom offset (adjust as needed)
+
         }
+
     func fetchDataForLineChart() {
         let db = Firestore.firestore()
         let collectionRef = db.collection("schedules")
@@ -47,32 +51,46 @@ class AnalysisViewController: UIViewController {
             }
 
             // Extract the dates and counts from the dateCountMap
-            self.dates = Array(dateCountMap.keys)
-            self.chartData = self.dates.map { date in
-                ChartDataEntry(x: Double(self.dates.firstIndex(of: date) ?? 0), y: Double(dateCountMap[date] ?? 0))
+            let sortedDates = Array(dateCountMap.keys).sorted()
+            self.chartData = sortedDates.enumerated().map { index, date in
+                ChartDataEntry(x: Double(index), y: Double(dateCountMap[date] ?? 0))
             }
+            self.dates = sortedDates
+
             // Call the method to set up and display the line chart
             self.setupLineChart()
         }
     }
 
+    func setupLineChart() {
+        let line1 = LineChartDataSet(entries: chartData, label: "Total Counts")
+        line1.colors = [NSUIColor.blue]
 
+        let data = LineChartData(dataSet: line1)
+        lineChartView.data = data
 
-        func setupLineChart() {
-            var lineChartEntry = [ChartDataEntry]()
-            for i in 0..<chartData.count {
-                let value = chartData[i]
-                lineChartEntry.append(value)
+        lineChartView.chartDescription.text = "Total Counts by Date"
+        lineChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: abbreviateDates(dates))
+        print(dates)
+        lineChartView.xAxis.labelRotationAngle = 0
+        lineChartView.xAxis.granularity = 1
+
+    }
+    
+    func abbreviateDates(_ dates: [String]) -> [String] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        return dates.map { dateStr in
+            if let date = dateFormatter.date(from: dateStr) {
+                dateFormatter.dateFormat = "MM/dd"
+                return dateFormatter.string(from: date)
             }
-
-            let line1 = LineChartDataSet(entries: lineChartEntry, label: "Total Counts")
-            line1.colors = [NSUIColor.blue]
-
-            let data = LineChartData(dataSet: line1)
-            lineChartView.data = data
-
-            lineChartView.chartDescription.text = "Total Counts by Date"
-            lineChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: dates)
-            lineChartView.xAxis.labelRotationAngle = -45
+            return dateStr
         }
+    }
+
+
 }
+
+
