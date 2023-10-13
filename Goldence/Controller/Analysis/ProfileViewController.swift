54bccda236d2b6843b1b68822fdef9faea0b3b7b
Ruby Jau <jau.ruby@gmail.com>
2023-10-13 15:26:23 +0800

    removing books in bookshelves

--- Goldence/Controller/Analysis/ProfileViewController.swift
+++ Goldence/Controller/Analysis/ProfileViewController.swift
@@ -11,7 +11,7 @@ import Firebase
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var analysisButton: UIButton!
-    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -23,9 +23,9 @@ class ProfileViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        editButton.layer.cornerRadius = 8
-        editButton.layer.borderColor = UIColor.hexStringToUIColor(hex: "6096ba").cgColor
-        editButton.layer.borderWidth = 1
+        deleteButton.layer.cornerRadius = 8
+        deleteButton.layer.borderColor = UIColor.red.cgColor
+        deleteButton.layer.borderWidth = 1
         
         analysisButton.layer.cornerRadius = 8
         analysisButton.layer.borderColor = UIColor.hexStringToUIColor(hex: "6096ba").cgColor
@@ -52,7 +52,65 @@ class ProfileViewController: UIViewController {
                 } else {
                     // Handle the case where the user document doesn't exist in Firebase
                     print("User document not found in Firebase.")
+                    self.nameLabel.text = "User"
                 }
             }
         }
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        // Retrieve the userIdentifier from UserDefaults
+            if let userIdentifier = UserDefaults.standard.string(forKey: "userIdentifier") {
+                let db = Firestore.firestore()
+                let usersCollection = db.collection("users")
+                // Reference to the user document
+                let userDocRef = usersCollection.document(userIdentifier)
+                userDocRef.getDocument { (document, error) in
+                    if let error = error {
+                        print("Error fetching user document: \(error.localizedDescription)")
+                        return
+                    }
+                    if let document = document, document.exists {
+                        // Check if bookshelfIDs and noteIDs arrays exist
+                        if let bookshelfIDs = document["bookshelfIDs"] as? [String],
+                           let noteIDs = document["noteIDs"] as? [String] {
+                            let batch = db.batch() // Create a batch for multiple deletes
+                            // Delete user's bookshelves
+                            let bookshelvesCollection = db.collection("bookshelves")
+                            for bookshelfID in bookshelfIDs {
+                                let bookshelfRef = bookshelvesCollection.document(bookshelfID)
+                                batch.deleteDocument(bookshelfRef)
+                            }
+                            // Delete user's notes
+                            let notesCollection = db.collection("notes")
+                            for noteID in noteIDs {
+                                let noteRef = notesCollection.document(noteID)
+                                batch.deleteDocument(noteRef)
+                            }
+                            // Delete the user document
+                            batch.deleteDocument(userDocRef)
+                            // Commit the batch operation
+                            batch.commit { (error) in
+                                if let error = error {
+                                    print("Error deleting user data: \(error.localizedDescription)")
+                                } else {
+                                    // Remove userIdentifier from UserDefaults
+                                    UserDefaults.standard.removeObject(forKey: "userIdentifier")
+                                    self.navigationController?.popViewController(animated: true)
+                                }
+                            }
+                        } else {
+                            // If the arrays don't exist, delete only the user document
+                            userDocRef.delete { (error) in
+                                if let error = error {
+                                    print("Error deleting user data: \(error.localizedDescription)")
+                                } else {
+                                    // Remove userIdentifier from UserDefaults
+                                    UserDefaults.standard.removeObject(forKey: "userIdentifier")
+                                    self.navigationController?.popViewController(animated: true)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+    }
 }
